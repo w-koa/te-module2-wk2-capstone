ee9dd6b65bb08f868fd49ef6f0a04107ef22e94e
Willis Koa <williskoa@yahoo.com>
2020-02-20 18:49:11 -0500

    Working on reservation methods

--- 09_Capstone/java/src/main/java/com/techelevator/view/JDBCReservationDAO.java
+++ 09_Capstone/java/src/main/java/com/techelevator/view/JDBCReservationDAO.java
@@ -1,10 +1,12 @@
 package com.techelevator.view;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import com.techelevator.model.Reservation;
 import com.techelevator.model.ReservationDAO;
@@ -18,9 +20,15 @@ public class JDBCReservationDAO implements ReservationDAO {
 	}
 	
 	@Override
-	public List<Reservation> searchForAvailableReservations() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Reservation> searchForAvailableReservations(String campgroundId, String startDate, String endDate) {
+		List<Reservation> availableReservations = new ArrayList<>();
+		
+		String sqlFindReservations = "SELECT reservation_id, site_id, name, from_date, "
+				+ "to_date, create_date FROM reservation WHERE site_id = ? AND to_date = ? AND from_date = ?";
+
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlFindReservations, campgroundId, startDate, endDate);
+		
+		return availableReservations;
 	}
 
 	@Override
